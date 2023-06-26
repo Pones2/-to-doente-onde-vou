a14cf729355421e22d3b07a9f314ad582607f1ea
Samuel Ferreira <samueldbferreira@gmail.com>
2023-06-25 22:32:35 -0300

    responsividade da home e do quiz

--- src/pages/home/startQuiz/StartQuiz.jsx
+++ src/pages/home/startQuiz/StartQuiz.jsx
@@ -3,15 +3,17 @@ import styles from "../Home.module.css";
 
 const StartQuiz = () => {
 	return (
-		<section className={`${styles.section} ${styles.start}`}>
-			<div className={`container ${styles.startContent}`}>
-				<h1>Tô Doente, Onde Vou?</h1>
-				<p>
-					Preencha nosso questionário de sintomas e te indicaremos a unidade de
-					saúde mais adequada para atendê-lo.
-				</p>
+		<section className={`${styles.start} ${styles.section}`}>
+			<div className={`container`}>
+				<div className={styles.startContent}>
+					<h1>Tô Doente, Onde Vou?</h1>
+					<p>
+						Preencha nosso questionário de sintomas e te indicaremos a unidade
+						de saúde mais adequada para atendê-lo.
+					</p>
 
-				<AddressBar />
+					<AddressBar />
+				</div>
 			</div>
 		</section>
 	);
